12dee89150a45f922f9505f00e2e6e850c70cfcf
nikuha <yoginika@yandex.ru>
2021-12-06 13:15:03 +0300

    lesson5 basket total price

--- basketapp/models.py
+++ basketapp/models.py
@@ -8,7 +8,7 @@ from mainapp.models import Product
 class Basket(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='basket')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.SmallIntegerField(verbose_name='количество', default=1)
+    quantity = models.SmallIntegerField(verbose_name='количество', default=0)
     add_datetime = models.DateTimeField(verbose_name='время', auto_now_add=True)
 
     @property
