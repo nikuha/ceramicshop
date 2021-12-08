12a7dcd898f10da498113753b560076d57a0f09f
nikuha <yoginika@yandex.ru>
2021-12-08 12:29:43 +0300

    lesson6 change quantity with ajax

--- basketapp/urls.py
+++ basketapp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', basketapp.basket, name='view'),
     path('add/<int:pk>/', basketapp.add_product, name='add'),
     path('remove/<int:pk>/', basketapp.remove_product, name='remove'),
+    path('update_quantity/<int:pk>/', basketapp.update_quantity, name='update_quantity'),
 
 ]
---
+++ static/js/basket.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    initBasketQuantity();
+});
+
+function initBasketQuantity() {
+
+    $('.plus button, .minus button').each(function () {
+
+        $(this).on('click', function () {
+
+            let container = $(this).closest('.quantity-container'),
+                quantity_div = container.find('.quantity'),
+                pk = quantity_div.attr('data-pk'),
+                max_quantity = quantity_div.attr('data-max'),
+                mode = $(this).closest('div').hasClass('plus') ? 'plus' : 'minus',
+                old_quantity = Number(quantity_div.html()),
+                new_quantity = mode === 'plus' ? old_quantity + 1 : old_quantity - 1;
+
+            if (new_quantity >= 1 && new_quantity <= max_quantity) {
+                $.ajax({
+                    type: "GET",
+                    url: '/basket/update_quantity/'+pk,
+                    data: {quantity: new_quantity},
+                    cache: false,
+
+                    success: function (data) {
+                        quantity_div.html(new_quantity);
+                        $('#product_cost'+pk).html(data.product_cost+'&nbsp;руб');
+                        $('#basket_total_quantity').html(data.total_quantity);
+                        $('#basket_total_cost').html(data.total_cost);
+                        $('#top_total_quantity').html(data.total_quantity);
+                        $('#top_total_cost').html(data.total_cost);
+                    },
+                    error: function (jqXHR) {
+                        alert('Что-то пошло не так!');
+                    }
+                });
+            }
+        });
+
+
+    });
+}
