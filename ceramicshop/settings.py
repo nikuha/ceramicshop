a52204be5e349af51ada07de34f11a41ca362c29
nikuha <yoginika@yandex.ru>
2022-01-25 20:22:03 +0300

    settings

--- ceramicshop/settings.py
+++ ceramicshop/settings.py
@@ -87,12 +87,21 @@ WSGI_APPLICATION = 'ceramicshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.getenv('DJANGO_DEVELOPMENT') == 'True':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'ceramicshop',
+            'USER': 'postgres',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
