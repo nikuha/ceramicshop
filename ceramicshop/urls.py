de5b8fb3c2f4b958c21bd057e3ff3d22c6f2dc48
nikuha <yoginika@yandex.ru>
2022-01-30 20:38:03 +0300

    install debug

--- ceramicshop/urls.py
+++ ceramicshop/urls.py
@@ -35,7 +35,7 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns += [path(r'__debug__/', include(debug_toolbar.urls))]
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'mainapp.views.handler404'
