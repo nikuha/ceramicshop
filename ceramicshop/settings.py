24a216fcb220abdce178a69d96cbdbebf674d64d
nikuha <yoginika@yandex.ru>
2022-02-06 15:08:27 +0300

    install debug

--- ceramicshop/settings.py
+++ ceramicshop/settings.py
@@ -58,7 +58,8 @@ MIDDLEWARE = [
 if DEBUG:
     INSTALLED_APPS.extend([
         'debug_toolbar',
-        'template_profiler_panel'
+        'template_profiler_panel',
+        'django_extensions',
     ])
     MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
 
@@ -165,10 +166,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
