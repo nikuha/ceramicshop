8f7e2c8e5ae29a7c152900aad0836d3cd203c41b
nikuha <yoginika@yandex.ru>
2022-02-09 18:31:59 +0300

    cache products

--- mainapp/products/urls.py
+++ mainapp/products/urls.py
@@ -5,6 +5,7 @@ app_name = 'products'
 
 urlpatterns = [
     path('', mainapp.HotProductsView.as_view(), name='index'),
+    path('all/', mainapp.AllProductsView.as_view(), name='all'),
     path('category_<int:pk>/', mainapp.ProductsView.as_view(), name='category'),
     path('<int:pk>/', mainapp.ProductView.as_view(), name='product')
 ]
